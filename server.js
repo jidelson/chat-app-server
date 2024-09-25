23d971a1806b4cef3043afbfd6669878efe68be8
Joe Idelson <joeidelson@gmail.com>
2024-09-25 09:08:45 -0500

    added exception handling

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const app = require("./app");
 
+process.on("uncaughtException", (err) => {
+    console.log(err);
+    process.exit(1);
+});
+
 const http = require("http"); // this comes from node.js
 
 const server = http.createServer(app);
@@ -9,4 +14,11 @@ const port = process.env.PORT || 8000;
 
 server.listen(port, () => {
     console.log(`App running on port ${port}`)
+});
+
+process.on("unhandledRejection", (err) => {
+    console.log(err);
+    server.close(() => {
+        process.exit(1);
+    });
 });
